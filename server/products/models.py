0249f5864769d6048af2398db56338f0692b9da9
AnastasiaRyabovolova <NastiaRyabov@gmail.com>
2018-09-28 00:02:03 +0300

    Add Products app with main model

---
+++ server/products/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+# Create your models here.
+
+
+class Product(models.Model):
+    title = models.CharField(_('title'), max_length=125)
+    description = models.CharField(
+        _('description'),
+        max_length=250,
+        blank=True,
+    )
+    is_active = models.BooleanField(_('active'), default=True)
+    image = models.ImageField(upload_to='products/product_{self.id}/')
+    price = models.DecimalField(
+        _('price'),
+        max_digits=15,
+        decimal_places=2,
+        default=0.00,
+    )
+
+    class Meta:
+        verbose_name = _('product')
+        verbose_name_plural = _('products')
